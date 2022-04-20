28bedc7c50fabbdbade7a91c19abbeff10ba9094
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-04-20 20:23:02 +0300

    Initial Commit

---
+++ EggAndTime/EggAndTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggAndTimeApp.swift
+//  EggAndTime
+//
+//  Created by Сашок on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct EggAndTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
